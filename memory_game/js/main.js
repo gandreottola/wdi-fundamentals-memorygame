b88a60a44ad152d5f00f3ae88329ac8c06445d55
Gabriel Andreottola <Gandreottola@Gabriels-MacBook-Air.local>
2019-06-04 12:22:27 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,21 @@ alert('Hello, friends.');
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne;
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo;
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
+function checkForMatch() {
 if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-		else {
-			alert("Sorry, try again.");
-		}
+console.log("You found a match!");
+}
+else {
+console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
